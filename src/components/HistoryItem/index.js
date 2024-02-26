6d02a9e35e5673468d1f35de0c137b1f2ff7d763
kedar gaikwad <kedarga1@gmail.com>
2024-02-26 09:04:14 +0000

    not showing the output

---
+++ src/components/HistoryItem/index.js
@@ -0,0 +1,32 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {historyDetails, deleteHistory} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyDetails
+
+  const onClickDelete = () => {
+    deleteHistory(id)
+  }
+
+  return (
+    <li className="list-item">
+      <div className="row-container">
+        <div className="row">
+          <p className="time">{timeAccessed}</p>
+          <img src={logoUrl} alt="domain logo" className="image" />
+          <p className="title">{title}</p>
+          <p className="domain">{domainUrl}</p>
+        </div>
+      </div>
+      <button type="button" onClick={onClickDelete} className="button">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/delete-img.png "
+          alt="delete"
+          className="delete"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default HistoryItem
